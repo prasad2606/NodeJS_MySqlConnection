175589b578a10ab79f6d87cf9b3d63d17ab2f9c2
prasad2606 <58550063+prasad2606@users.noreply.github.com>
2019-12-15 20:48:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -10,16 +10,16 @@ database:"sampledb"
 });
 
 con.connect();
-var ibmcat = "select * from XXIBM_PRODUCT_CATALOG";
-con.query(ibmcat,function(err,result,details){
-    if(err){
-        console.log("Hata var!");
-       }
-   else{
-        console.log(result);
-         console.log(details); // for details 
-    }
-});
+//var ibmcat = "select * from XXIBM_PRODUCT_CATALOG";
+//con.query(ibmcat,function(err,result,details){
+//    if(err){
+//        console.log("Hata var!");
+//       }
+//   else{
+//        console.log(result);
+//         console.log(details); // for details 
+ //   }
+//});
 con.end();
 
-app.listen(8000);
+app.listen(8080);
